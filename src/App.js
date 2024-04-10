f5a6e4774dcd13b52c36d8932044601d0b6f9eac
Jace <jrcosmoz@gmail.com>
2024-04-10 16:28:20 -0230

    Main page fixes

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ import AuthorsPage from './pages/AuthorsPage';
 import AuthorForm from './pages/AuthorForm';
 import BooksPage from './pages/BooksPage';
 import BookForm from './pages/BookForm';
-import UpdateAuthorForm from './pages/UpdateAuthorForm';  
-import UpdateBookForm from './pages/UpdateBookForm';      
-import DeleteAuthorForm from './pages/DeleteAuthorForm';  
-import DeleteBookForm from './pages/DeleteBookForm';      
+import UpdateAuthorForm from './pages/UpdateAuthorForm';
+import UpdateBookForm from './pages/UpdateBookForm';
+import DeleteAuthorForm from './pages/DeleteAuthorForm';
+import DeleteBookForm from './pages/DeleteBookForm';
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={MainPage} />
-                <Route path="/authors" element={AuthorsPage} />
-                <Route path="/authors/new" element={AuthorForm} />
-                <Route path="/authors/update" element={UpdateAuthorForm} />
-                <Route path="/authors/delete" element={DeleteAuthorForm} />
-                <Route path="/books" element={BooksPage} />
-                <Route path="/books/new" element={BookForm} />
-                <Route path="/books/update" element={UpdateBookForm} />
-                <Route path="/books/delete" element={DeleteBookForm} />
+                <Route path="/" element={<MainPage />} />
+                <Route path="/authors" element={<AuthorsPage />} />
+                <Route path="/authors/new" element={<AuthorForm />} />
+                <Route path="/authors/update" element={<UpdateAuthorForm />} />
+                <Route path="/authors/delete" element={<DeleteAuthorForm />} />
+                <Route path="/books" element={<BooksPage />} />
+                <Route path="/books/new" element={<BookForm />} />
+                <Route path="/books/update" element={<UpdateBookForm />} />
+                <Route path="/books/delete" element={<DeleteBookForm />} />
             </Routes>
         </Router>
     );
